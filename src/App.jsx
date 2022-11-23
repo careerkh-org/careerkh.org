975b8ced544145ce108583b65d262cd19ee1de17
Mekmun-Sopheaktra <mekmunsopheaktra@gmail.com>
2022-11-23 13:24:33 +0700

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,19 @@
 import { useState } from 'react'
+import styles from "./style.js";
+import  { home } from "./components";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>CareerKh</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="bg-primary w-full overflow-hidden">
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+            <div className={`${styles.boxWidth}`}>
+               <div>
+                home
+               </div>
+            </div>
+        </div>
     </div>
   )
 }
