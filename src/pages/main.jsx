1a0adacf1e55e19c2b035a48e4ee91ca9e2cd0e5
lysokheng <sokheng6485@gmail.com>
2022-11-23 13:51:53 +0700

    update

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '../index.css'
 import NavBar from '../components/NavBar'
+import Error404Page from '../components/Error404Page'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NavBar />
-
   </React.StrictMode>
 )
