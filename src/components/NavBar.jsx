e020b5a1d4f7f2a6d63bf198d12b0055973952ec
lysokheng <sokheng6485@gmail.com>
2022-12-11 19:56:22 +0700

    home-v1.1

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -164,6 +164,7 @@ export default function NavBar() {
                                     Forum
                                 </Disclosure.Button>
                             </NavLink>
+                            
                         </div>
                     </Disclosure.Panel>
                 </>
