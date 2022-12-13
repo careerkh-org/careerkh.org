1d7bd67833c6f7490d13fbe524fde73c5f4e3f34
Mekmun Sopheaktra <Mekmunsopheaktra@gmail.com>
2022-12-13 16:25:47 +0700

    login

---
+++ src/base.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default firebaseConfig;
---
+++ src/pages/Auth.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Login from '../components/Login'
+const Auth = () => {
+  return (
+    <>
+    <Login/>
+    </>
+  )
+}
+
+export default Auth
