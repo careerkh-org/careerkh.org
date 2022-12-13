abb0dab59ab9ef6425332e9d6ae6a22a02b38de0
Mekmun Sopheaktra <Mekmunsopheaktra@gmail.com>
2022-12-13 23:13:57 +0700

    update img

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -171,7 +171,7 @@ export default function NavBar() {
                             </NavLink>
 
                     
-                            <NavLink to="/login">
+                            <NavLink to="/signin">
                                 <Disclosure.Button
                                     type="button"
                                     className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800 font-inter"
