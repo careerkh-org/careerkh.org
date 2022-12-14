dbbc946ff8b8c7359d44724fd70b909586d57c05
lysokheng <sokheng6485@gmail.com>
2022-12-14 20:54:30 +0700

    using strapi api

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import NavBar from '../components/NavBar'
 import { Route, Router, Routes } from 'react-router-dom'
 import Home from './Home'
@@ -10,16 +9,16 @@ import Forum from './Forum'
 const main = () => {
   return (
     <React.StrictMode>
-    <NavBar />
-    <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/careers' element={<Careers/>}></Route>
-      <Route path='/roadmap' element={<Roadmap/>}></Route>
-      <Route path='/forum' element={<Forum/>}></Route>
-    </Routes>
-    
-  </React.StrictMode>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/careers' element={<Careers />}></Route>
+        <Route path='/roadmap' element={<Roadmap />}></Route>
+        <Route path='/forum' element={<Forum />}></Route>
+      </Routes>
+
+    </React.StrictMode>
   );
 };
-  
+
 export default main;
