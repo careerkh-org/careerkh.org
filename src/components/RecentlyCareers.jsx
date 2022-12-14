19dc825bd6b666b5ec38ad2343cf2d5d6b098ba4
lysokheng <sokheng6485@gmail.com>
2022-12-14 22:26:59 +0700

    update recently career

--- src/components/RecentlyCareers.jsx
+++ src/components/RecentlyCareers.jsx
@@ -48,21 +48,20 @@ export default function RecentlyCareers() {
                                             </a>
                                         </p>
                                         <a href={career.attributes.href} className="mt-2 block">
-                                            <p className="text-xl font-semibold text-gray-900">{career.attributes.caption}</p>
-                                            <p className="my-3 text-base text-gray-500">{career.attributes.position}</p>
+                                            <p className="text-xl font-semibold text-gray-900">{career.attributes.position}</p>
+                                            <p className="my-3 text-base text-gray-500">{career.attributes.caption}</p>
 
                                             <div className="flex align-middle items-center text-center py-2">
                                                 <svg class="w-6 h-6 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
                                                 <p className="mx-3 text-base text-gray-500">{career.attributes.term}</p>
+
                                             </div>
 
                                             <div className="flex align-middle items-center text-center py-2">
                                                 <svg class="w-6 h-6 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>                                            <p className="mx-3 text-base text-gray-500">{career.attributes.location}</p>
                                             </div>
 
-                                            <div className="flex align-middle items-center text-center py-2">
-                                                <svg class="w-6 h-6 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>                                            <p className="mx-3 text-base text-gray-500">{career.attributes.salary} $</p>
-                                            </div>
+
 
                                             <div className="flex align-middle items-center text-center py-2">
                                                 <svg class="w-6 h-6 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>                                            <p className="mx-3 text-base text-gray-500">{career.attributes.date}</p>
