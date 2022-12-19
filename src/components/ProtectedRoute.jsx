d439719314a8a80f294da0f234f30c099ab23a1e
Mekmun Sopheaktra <Mekmunsopheaktra@gmail.com>
2022-12-19 13:40:20 +0700

    login

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -6,7 +6,7 @@ const ProtectedRoute = ({ children }) => {
   const { user } = UserAuth();
 
   if (!user) {
-    return <Navigate to='/login' />;
+    return <Navigate to='/login' replace/>;
   }
   return children;
 };
