2fa606d3b8fc3bde237d33ab8596ac5807d323fd
Mekmun Sopheaktra <Mekmunsopheaktra@gmail.com>
2023-01-18 14:53:46 +0700

    FAQ

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -41,7 +41,7 @@ const faqs = [
 export default function FAQ() {
     return (
         <div className="bg-white">
-            <div className="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+            <div className="mx-auto max-w-7xl py-16 px-4 sm:py-12 sm:px-6 lg:px-8">
                 <h2 className="text-center text-3xl font-bold tracking-tight text-gray-900">Frequently asked questions</h2>
                 <div className="mt-12">
                     <dl className="space-y-10 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-12 md:space-y-0 lg:grid-cols-3">
